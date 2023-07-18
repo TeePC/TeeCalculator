690aba08b4655675590dc415703be1152c7d6c9e
TeePC <andretthom@gmail.com>
2023-07-18 18:17:53 -0400

    Added basic calculator operator functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+let number_1;
+let number_2;
+let operator;
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+
+
+
+function operate(num1, num2, operator) {
+
+    if (operator === 'add') {
+        add(num1, num2);
+
+    } else if ( operator === 'subtract') {
+        subtract(num1, num2 ); 
+
+    } else if ( operator === 'multiply') {
+        multiply(num1, num2 ); 
+
+    } else if ( operator === 'divide') {
+        divide(num1, num2 ); 
+
+    }
+
+}
